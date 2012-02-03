e11a8051c591dc2b164921b1d888f8e4151c716f
Nigel Babu <nigelbabu@gmail.com>
2012-02-03 10:25:26 +0530

    Added static files

---
+++ boxoffice/static/js/scripts.js
@@ -0,0 +1,13 @@
+// ROT13 link handler
+$(function() {
+  $("a.rot13").each(function() {
+    if ($(this).attr('data-href')) {
+      var decoded = $(this).attr('data-href').replace(/[a-zA-Z]/g, function(c) {
+        return String.fromCharCode((c<="Z"?90:122)>=(c=c.charCodeAt(0)+13)?c:c-26);
+        });
+      $(this).attr('href', decoded);
+      $(this).removeAttr('data-href');
+      $(this).removeClass('rot13');
+    };
+  });
+});
