8f5d3bb8eb07b89696f5f54ac758ed7faa96aa51
Shreyas Satish <shreyas.satish@gmail.com>
2016-02-01 19:24:53 +0530

    rm customer

--- boxoffice/models/user.py
+++ boxoffice/models/user.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 from flask import g
 from flask.ext.lastuser.sqlalchemy import UserBase2, ProfileBase
-from boxoffice.models import db, IdMixin
+from boxoffice.models import db
 
 
-__all__ = ['User', 'Organization', 'Customer']
+__all__ = ['User', 'Organization']
 
 
 class User(UserBase2, db.Model):
@@ -21,19 +21,3 @@ def default_user(context):
 
 class Organization(ProfileBase, db.Model):
     __tablename__ = 'organization'
-
-
-class Customer(IdMixin, db.Model):
-    __tablename__ = 'customer'
-    __uuid_primary_key__ = True
-
-    fullname = db.Column(db.Unicode(80), nullable=False)
-    #: Unvalidated email address
-    email = db.Column(db.Unicode(254), nullable=False)
-    #: Unvalidated phone number
-    phone = db.Column(db.Unicode(80), nullable=True)
-    email_verified = db.Column(db.Boolean, default=False, nullable=False)
-    address = db.Column(db.Unicode(256), nullable=False)
-
-    account_id = db.Column(None, db.ForeignKey('customer.id'))
-    account = db.relationship("Account", back_populates="customer")
