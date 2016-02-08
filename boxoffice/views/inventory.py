8164868943f9a11f706892172e446e497c550a39
Shreyas Satish <shreyas.satish@gmail.com>
2016-02-08 18:23:06 +0530

    added cors handler

--- boxoffice/views/inventory.py
+++ boxoffice/views/inventory.py
@@ -1,8 +1,10 @@
 from flask import redirect, url_for, render_template, request
+from flask.ext.cors import cross_origin
 from coaster.views import load_models, jsonp
 from boxoffice import app
 from boxoffice.models import Organization, Item, Category, Inventory, Price
 
+ALLOWED_ORIGINS = ['http://shreyas-wlan.dev:8000']
 
 def item_json(item):
     price = Price.current(item)
@@ -47,17 +49,19 @@ def inventory(organization, inventory):
     (Organization, {'name': 'organization'}, 'organization'),
     (Inventory, {'name': 'inventory'}, 'inventory')
     )
+@cross_origin(origins=ALLOWED_ORIGINS, methods=['GET', 'OPTIONS', 'POST'])
 def order(organization, inventory):
     # change to get user
     user = User.query.first()
     order = Order(user=user, inventory=inventory)
     order.calculate(request.form.line_items)
+    db.session.add(order)
     payment = Payment(pg_payment_id=request.form.pg_payment_id, order=order)
     db.session.add(payment)
     # payment.capture
     # Transaction(payment=payment)
     # return jsonify(...)
+    db.session.commit()
     return jsonp(**{
-        'html': render_template('boxoffice.html'),
-        'categories': [category_json(category) for category in inventory.categories]
+        'order_id': order.id
         })
