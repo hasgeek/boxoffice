750d37b2bc104aafe3a5ed4cfc68f9bf46ea674e
Shreyas Satish <shreyas.satish@gmail.com>
2016-02-09 17:45:44 +0530

    pass payment_url

--- boxoffice/views/inventory.py
+++ boxoffice/views/inventory.py
@@ -59,10 +59,13 @@ def order(organization, inventory):
     order = Order(user=user, inventory=inventory)
     form_values = request.form.to_dict().keys()
     if form_values:
-        order.calculate(json.loads(form_values[0]).get('line_items'))
+        form_values_json = json.loads(form_values[0])
+        # user = User.find_or_create(email=form_values_json.get('email'), phone=form_values_json.get('phone'), name=form_values_json.get('name'))
+        # order.user = user
+        order.calculate(form_values_json.get('line_items'))
         db.session.add(order)
         db.session.commit()
-        return jsonify(code=200, order_id=order.id)
+        return jsonify(code=200, order_id=order.id, payment_url=url_for('payment', order=order.id))
 
 
 @app.route('/<order>/payment', methods=['GET', 'OPTIONS', 'POST'])
@@ -86,6 +89,26 @@ def payment(order):
         return jsonify(code=200)
     elif payment.status == PAYMENT_TYPES.FAILED:
         return jsonify(code=402)
+
+
+@app.route('/<order>/cancel', methods=['POST'])
+@load_models(
+    (Order, {'id': 'order'}, 'order')
+    )
+@cross_origin(allow_headers=['Content-Type'])
+def cancel(order):
+    # line_items_dict = [{id: '', quantity: 3, name: ''}, {id: '', quantity: 4, name: ''}]
+    # for line_item_dict in line_items_dict:
+    #     line_item = LineItem.get(line_item_dict.get('id'))
+    #    if line_item_dict.get('quantity') == 0:
+    #        line_item.cancel()
+    #    elif line_item_dict('quantity') < line_item.quantity:
+    #        line_item.cancel()
+    #        new_line_item = LineItem(quantity=line_item_dict('quantity'), order=order, item=line_item.item)
+    #        db.session.add(new_line_item)
+    # db.session.commit()
+    # render_template('invoice.html', order=order)
+    pass
 
 
 @app.route('/<order>/invoice', methods=['GET'])
