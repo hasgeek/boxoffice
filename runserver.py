8d475f188c6740b71d189d17eff0e3dee924b88e
Karthik Balakrishnan <karthikb351@gmail.com>
2016-02-19 15:54:07 +0530

    Build status

--- runserver.py
+++ runserver.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
-from boxoffice import app, init_for, models
+from boxoffice import app, init_for
 from boxoffice.models import *
+
 init_for('dev')
 
 app.run('0.0.0.0', 6500, debug=True, threaded=True)
