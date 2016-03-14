800843003eab405bf80d17f0bf5f199d7e322f92
Shreyas Satish <shreyas.satish@gmail.com>
2016-03-14 12:37:59 +0530

    fixed indent

--- boxoffice/models/item.py
+++ boxoffice/models/item.py
@@ -10,20 +10,16 @@ class Item(BaseScopedNameMixin, db.Model):
     __uuid_primary_key__ = True
     __tableargs__ = (db.UniqueConstraint('item_collection_id', 'name'),
         db.CheckConstraint('quantity_available <= quantity_total',
-                          'item_quantity_available_lte_quantity_total_check'))
+            'item_quantity_available_lte_quantity_total_check'))
 
     description = db.Column(db.Unicode(2500), nullable=False)
 
-    item_collection_id = db.Column(None,
-                                   db.ForeignKey('item_collection.id'),
-                                   nullable=False)
-    item_collection = db.relationship(ItemCollection,
-                                      backref=db.backref('items', cascade='all, delete-orphan'))  # noqa
+    item_collection_id = db.Column(None, db.ForeignKey('item_collection.id'), nullable=False)
+    item_collection = db.relationship(ItemCollection, backref=db.backref('items', cascade='all, delete-orphan'))
     parent = db.synonym('item_collection')
 
     category_id = db.Column(None, db.ForeignKey('category.id'), nullable=False)
-    category = db.relationship(Category,
-                               backref=db.backref('items', cascade='all, delete-orphan'))  # noqa
+    category = db.relationship(Category, backref=db.backref('items', cascade='all, delete-orphan'))
 
     quantity_available = db.Column(db.Integer, default=0, nullable=False)
     quantity_total = db.Column(db.Integer, default=0, nullable=False)
