5c0cd439e8966c45978e2e02dfce2371e480f253
Shreyas Satish <shreyas.satish@gmail.com>
2016-03-21 22:23:08 +0530

    rm error handler

--- boxoffice/views/custom_exceptions.py
+++ boxoffice/views/custom_exceptions.py
@@ -1,6 +1,3 @@
-from flask import jsonify
-from .. import app
-
 
 class APIError(Exception):
 
@@ -8,8 +5,3 @@ class APIError(Exception):
         Exception.__init__(self)
         self.message = message
         self.status_code = status_code
-
-
-@app.errorhandler(APIError)
-def handle_api_error(error):
-    return jsonify(message=error.message), error.status_code
