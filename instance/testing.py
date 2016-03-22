25fb035c54cb1be156f56f16ddd9e8d18d9610e8
Shreyas Satish <shreyas.satish@gmail.com>
2016-03-22 10:49:30 +0530

    added dummy key for travis

--- instance/testing.py
+++ instance/testing.py
@@ -1,5 +1,6 @@
 import os
 #: Database backend
+SECRET_KEY = 'testkey'
 SQLALCHEMY_DATABASE_URI = 'postgres://127.0.0.1/boxoffice_testing'
 SERVER_NAME = 'boxoffice.travis.dev:6500'
 BASE_URL = 'http://' + SERVER_NAME
