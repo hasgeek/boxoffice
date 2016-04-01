fc188b70695c4f5b240e41ef65905ab0c02c11d3
Shreyas Satish <shreyas.satish@gmail.com>
2016-04-01 12:52:46 +0530

    rm noqa, use binary=false for UUID

--- alembic/versions/45de268cd444_add_discount_policy_id_to_price.py
+++ alembic/versions/45de268cd444_add_discount_policy_id_to_price.py
@@ -18,7 +18,7 @@ import sqlalchemy_utils
 def upgrade():
     op.add_column('price', sa.Column('discount_policy_id', sqlalchemy_utils.types.uuid.UUIDType(binary=False), nullable=True))
     op.create_unique_constraint('price_item_id_discount_policy_id_key', 'price', ['item_id', 'discount_policy_id'])
-    op.create_foreign_key('price_discount_policy_id_key', 'price', 'discount_policy', ['discount_policy_id'], ['id'])
+    op.create_foreign_key('price_discount_policy_id_fkey', 'price', 'discount_policy', ['discount_policy_id'], ['id'])
     op.alter_column('discount_policy', 'percentage',
                existing_type=sa.INTEGER,
                nullable=True)
@@ -26,7 +26,10 @@ def upgrade():
 
 
 def downgrade():
-    op.drop_constraint('price_discount_policy_id_key', 'price', type_='foreignkey')
+    op.drop_constraint('price_discount_policy_id_fkey', 'price', type_='foreignkey')
     op.drop_constraint('price_item_id_discount_policy_id_key', 'price', type_='unique')
     op.drop_column('price', 'discount_policy_id')
     op.drop_column('discount_policy', 'is_price_based')
+    op.alter_column('discount_policy', 'percentage',
+               existing_type=sa.INTEGER,
+               nullable=True)
