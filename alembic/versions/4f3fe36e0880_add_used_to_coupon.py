9ce91170b2196d52afb6b13228ec374543139eef
Shreyas Satish <shreyas.satish@gmail.com>
2016-04-04 15:30:20 +0530

    added missing constraints

--- alembic/versions/4f3fe36e0880_add_used_to_coupon.py
+++ alembic/versions/4f3fe36e0880_add_used_to_coupon.py
@@ -17,7 +17,11 @@ from sqlalchemy.dialects import postgresql
 
 def upgrade():
     op.add_column('discount_coupon', sa.Column('used', sa.Boolean(), nullable=True))
+    op.drop_constraint('discount_policy_item_quantity_check', 'discount_policy')
+    op.drop_constraint('discount_coupon_quantity_check', 'discount_coupon')
 
 
 def downgrade():
     op.drop_column('discount_coupon', 'used')
+    op.create_check_constraint('discount_coupon_quantity_check', 'discount_coupon', u'quantity_available <= quantity_total')
+    op.create_check_constraint('discount_policy_item_quantity_check', 'discount_policy', u'item_quantity_min <= item_quantity_max')
