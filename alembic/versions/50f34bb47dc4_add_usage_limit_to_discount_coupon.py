ea58fdb6e96c24f8e2e5bb508c8190035bc45020
Shreyas Satish <shreyas.satish@gmail.com>
2016-04-08 07:59:41 +0530

    fixed migration

--- alembic/versions/50f34bb47dc4_add_usage_limit_to_discount_coupon.py
+++ alembic/versions/50f34bb47dc4_add_usage_limit_to_discount_coupon.py
@@ -27,9 +27,11 @@ line_item = table('line_item',
 
 
 def upgrade():
-    op.add_column('discount_coupon', sa.Column('usage_limit', sa.Integer(), nullable=True, server_default='1'))
+    op.add_column('discount_coupon', sa.Column('usage_limit', sa.Integer(), nullable=False, server_default='1'))
     op.add_column('discount_coupon', sa.Column('used_count', sa.Integer(), nullable=False, server_default='0'))
     op.execute(discount_coupon.update().where(line_item.c.discount_coupon_id == discount_coupon.c.id).values({'used_count': 1}))  # noqa
+    op.alter_column('discount_coupon', 'usage_limit', server_default=None)
+    op.alter_column('discount_coupon', 'used_count', server_default=None)
     op.drop_column('discount_coupon', 'used')
 
 
@@ -38,3 +40,4 @@ def downgrade():
     op.execute(discount_coupon.update().where(line_item.c.discount_coupon_id == discount_coupon.c.id).values({'used': True}))  # noqa
     op.alter_column('discount_coupon', 'used', server_default=None)
     op.drop_column('discount_coupon', 'usage_limit')
+    op.drop_column('discount_coupon', 'used_count')
