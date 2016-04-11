2a03c7289eb80644e42fb371229bbaed4598eb6d
Vidya Ramakrishnan <vidya@hasgeek.in>
2016-04-11 20:14:57 +0530

    Change to select.

--- boxoffice/views/participant.py
+++ boxoffice/views/participant.py
@@ -26,16 +26,17 @@ def assign(order):
     assignee = Assignee.query.filter_by(email=assignee_dict.get('email')).one_or_none()
     if not assignee:
         item_assignee_details = line_item.item.assignee_details
-        assignee_details = {}
+        assignee_details = {line_item.item_id: {}}
         for key in item_assignee_details.keys():
             assignee_details[key] = assignee_dict.get(key)
         print('assignee_details', assignee_details)
-        assignee = Assignee(email=assignee_dict.get('email'), fullname=assignee_dict.get('fullname'), phone=assignee_dict.get('phone'), details=assignee_details)
+        assignee = Assignee(email=assignee_dict.get('email'), fullname=assignee_dict.get('fullname'),
+            phone=assignee_dict.get('phone'), details=assignee_details[unicode(line_item.item_id)])
         db.session.add(assignee)
     else:
         item_assignee_details = line_item.item.assignee_details
         for key in item_assignee_details.keys():
-            assignee.details[key] = assignee_dict.get(key)
+            assignee.details[unicode(line_item.item_id)][key] = assignee_dict.get(key)
     line_item.assignee = assignee
     db.session.add(line_item)
     db.session.commit()
