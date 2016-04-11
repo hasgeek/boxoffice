e05b2c4afc1aa30d08880d142923d8ee28c79da3
Shreyas Satish <shreyas.satish@gmail.com>
2016-04-11 23:25:52 +0530

    reflected changes in model

--- boxoffice/views/participant.py
+++ boxoffice/views/participant.py
@@ -1,11 +1,10 @@
-from flask import url_for, request, jsonify, render_template, make_response
+from flask import request, jsonify, make_response
 from .. import app
 from ..models import db
-from ..models import ItemCollection, LineItem, Item, DiscountCoupon, DiscountPolicy, Assignee
-from ..models import Order, OnlinePayment, PaymentTransaction, User, CURRENCY
-from coaster.views import render_with, load_models
-from utils import xhr_only, cors
-
+from ..models import LineItem, Assignee
+from ..models import Order
+from coaster.views import load_models
+from utils import xhr_only
 
 
 @app.route('/participant/<order>/assign', methods=['GET', 'OPTIONS', 'POST'])
@@ -20,24 +19,13 @@ def assign(order):
     if not request.json or not request.json.get('attendee'):
         return make_response(jsonify(message='<Missing></Missing> Attendee details'), 400)
     assignee_dict = request.json.get('attendee')
-    line_item_id = request.json.get('line_item_id')
-    print('line_item_id', line_item_id)
     line_item = LineItem.query.get(request.json.get('line_item_id'))
-    assignee = Assignee.query.filter_by(email=assignee_dict.get('email')).one_or_none()
-    if not assignee:
-        item_assignee_details = line_item.item.assignee_details
-        assignee_details = {line_item.item_id: {}}
-        for key in item_assignee_details.keys():
-            assignee_details[key] = assignee_dict.get(key)
-        print('assignee_details', assignee_details)
-        assignee = Assignee(email=assignee_dict.get('email'), fullname=assignee_dict.get('fullname'),
-            phone=assignee_dict.get('phone'), details=assignee_details)
-        db.session.add(assignee)
-    else:
-        item_assignee_details = line_item.item.assignee_details
-        for key in item_assignee_details.keys():
-            assignee.details[key] = assignee_dict.get(key)
-    line_item.assignee = assignee
-    db.session.add(line_item)
+    item_assignee_details = line_item.item.assignee_details
+    assignee_details = {}
+    for key in item_assignee_details.keys():
+        assignee_details[key] = assignee_dict.get(key)
+    assignee = Assignee(current=True, email=assignee_dict.get('email'), fullname=assignee_dict.get('fullname'),
+        phone=assignee_dict.get('phone'), details=assignee_details, line_item=line_item)
+    db.session.add(assignee)
     db.session.commit()
     return make_response(jsonify(message="Ticket assigned"), 201)
