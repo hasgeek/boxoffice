a82d87d5bcd2bdb18e967d3acbf8066db83bc37f
Vidya Ramakrishnan <vidya@hasgeek.in>
2016-04-12 16:47:02 +0530

    Add BASE_URL to send_participant_assignment_mail.

--- boxoffice/mailclient.py
+++ boxoffice/mailclient.py
@@ -25,9 +25,8 @@ def send_receipt_email(order_id, subject="Thank you for your order!"):
 def send_participant_assignment_mail(order_id, subject="Please tell us who's coming!"):
     with app.test_request_context():
         order = Order.query.get(order_id)
-        base_url=request.url_root.strip('/')
         msg = Message(subject=subject, recipients=[order.buyer_email], bcc=[order.organization.contact_email])
-        html = email_transform(render_template('participant_assignment_mail.html', base_url=base_url, order=order, org=order.organization))
+        html = email_transform(render_template('participant_assignment_mail.html', base_url=app.config['BASE_URL'], order=order, org=order.organization))
         msg.html = html
         msg.body = html2text(html)
         mail.send(msg)
