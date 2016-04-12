1555e687d4446bab47001f2963a8b6792639aefc
Vidya Ramakrishnan <vidya@hasgeek.in>
2016-04-12 15:32:31 +0530

    Add participant assignment mail.

--- boxoffice/mailclient.py
+++ boxoffice/mailclient.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from flask import render_template
+from flask import render_template, request
 from flask.ext.mail import Message
 from html2text import html2text
 from premailer import transform as email_transform
@@ -24,8 +24,9 @@ def send_receipt_email(order_id, subject="Thank you for your order!"):
 def send_participant_assignment_mail(order_id, subject="Please tell us who's coming!"):
     with app.test_request_context():
         order = Order.query.get(order_id)
+        base_url=request.url_root.strip('/')
         msg = Message(subject=subject, recipients=[order.buyer_email], bcc=[order.organization.contact_email])
-        html = email_transform(render_template('participant_assignment_mail.html', order=order, org=order.organization))
+        html = email_transform(render_template('participant_assignment_mail.html', base_url=base_url, order=order, org=order.organization))
         msg.html = html
         msg.body = html2text(html)
         mail.send(msg)
