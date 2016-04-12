3079993eb54da102961fc06b2908eebd40d1fb2e
Sindhu S <sindhus@live.in>
2016-04-12 16:00:23 +0530

    Fix for DiscountPolicy query by item_collection

--- boxoffice/extapi/slack.py
+++ boxoffice/extapi/slack.py
@@ -6,9 +6,9 @@ import json
 from tabulate import tabulate
 from rq.decorators import job
 
-def discount_stats():
+def discount_stats(item_collection):
     stats = []
-    for policy in DiscountPolicy.query.all():
+    for policy in DiscountPolicy.query.get(item_collection):
         line_item_count = line_item_count=LineItem.query.filter(LineItem.item_id.in_([li_item.id for li_item in policy.items]), LineItem.status==LINE_ITEM_STATUS.CONFIRMED, LineItem.discount_policy == policy).count()
         stats.append([policy.title, line_item_count])
     return tabulate(stats, headers=["Policy Title", "Count"])
@@ -33,7 +33,8 @@ def post_stats(id, webhook_url):
     with app.test_request_context():
         item_collection = ItemCollection.query.get(id)
         tickets = ticket_stats(item_collection)
-        stats = ":moneybag: " + item_collection.title + "\n```" + tickets + "\n\n" + discount_stats() + "```"
+        discounts = discount_stats(item_collection)
+        stats = ":moneybag: " + item_collection.title + "\n```" + tickets + "\n\n" + discounts + "```"
         data = {"username": "boxoffice", "text": stats}
         response = requests.post(webhook_url, data=json.dumps(data))
 
