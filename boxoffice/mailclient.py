6b57ea8bd631b2c8e7b1f6e3347cf93c9d2e006f
Shreyas Satish <shreyas.satish@gmail.com>
2016-04-12 14:47:15 +0530

    added mail handler

--- boxoffice/mailclient.py
+++ boxoffice/mailclient.py
@@ -19,3 +19,13 @@ def send_receipt_email(order_id, subject="Thank you for your order!"):
         msg.html = html
         msg.body = html2text(html)
         mail.send(msg)
+
+
+def send_participant_assignment_mail(order_id, subject="Please tell us who's coming!"):
+    with app.test_request_context():
+        order = Order.query.get(order_id)
+        msg = Message(subject=subject, recipients=[order.buyer_email], bcc=[order.organization.contact_email])
+        html = email_transform(render_template('participant_assignment_mail.html', order=order, org=order.organization))
+        msg.html = html
+        msg.body = html2text(html)
+        mail.send(msg)
