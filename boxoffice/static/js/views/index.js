fd1b7341da7e3900424987d4548f4c41dd7765a2
Shreyas Satish <shreyas.satish@gmail.com>
2016-05-23 16:04:10 +0530

    rm unnecessary conditional init

--- boxoffice/static/js/views/index.js
+++ boxoffice/static/js/views/index.js
@@ -4,27 +4,19 @@ import {IndexTemplate} from '../templates/index.html.js';
 
 export const IndexView = {
   render: function() {
-    console.log(!this.ractive);
-    if (!this.indexModel) {
-      this.indexModel = new IndexModel();
-    }
+    this.indexModel = new IndexModel();
     this.indexModel.fetch().then(function(data){
-      if (!this.ractive) {
-        console.log("ractive")
-        this.ractive = new Ractive({
-          el: '#main-content-area',
-          template: IndexTemplate,
-          data: {
-            orgs: data.orgs
-          }
-        });
-        this.ractive.on('navigate', function(event, method){
-          // console.log(event.context.url);
-          eventBus.trigger('navigate', event.context.url);
-        });
-      } else {
-        this.ractive.render();
-      }
+      this.ractive = new Ractive({
+        el: '#main-content-area',
+        template: IndexTemplate,
+        data: {
+          orgs: data.orgs
+        }
+      });
+      this.ractive.on('navigate', function(event, method){
+        // console.log(event.context.url);
+        eventBus.trigger('navigate', event.context.url);
+      });
     })
   }
 }
--- boxoffice/static/js/views/org.js
+++ boxoffice/static/js/views/org.js
@@ -5,30 +5,23 @@ import {orgTemplate} from '../templates/org.html.js';
 
 export const OrgView = {
   render: function(org) {
-    if (!this.orgModel) {
-      this.orgModel = new OrgModel({
-        name: org.name
-      });
-    }
+    this.orgModel = new OrgModel({
+      name: org.name
+    });
     this.orgModel.fetch().then(function(data){
-      if (!this.ractive) {
-        this.ractive = new Ractive({
-          el: '#main-content-area',
-          // template: '#org-content-template',
-          template: orgTemplate,
-          data: {
-            name: data.name,
-            item_collections: data.item_collections
-          }
-        });
-        this.ractive.on('navigate', function(event, method){
-          // console.log(event.context.url);
-          eventBus.trigger('navigate', event.context.url);
-        });
-
-      } else {
-        this.ractive.render();
-      }
+      this.ractive = new Ractive({
+        el: '#main-content-area',
+        // template: '#org-content-template',
+        template: orgTemplate,
+        data: {
+          name: data.name,
+          item_collections: data.item_collections
+        }
+      });
+      this.ractive.on('navigate', function(event, method){
+        // console.log(event.context.url);
+        eventBus.trigger('navigate', event.context.url);
+      });
     });
 
   }
