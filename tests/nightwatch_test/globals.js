5699b9247236f3cb576458eeb146ffda8eb1d52e
Vidya Ramakrishnan <vidya@hasgeek.in>
2016-08-17 13:21:56 +0530

    Add global.js file.

---
+++ tests/nightwatch_test/globals.js
@@ -0,0 +1,15 @@
+module.exports = {
+  delay: 1500,
+  userName: 'Test',
+  userEmail: 'test@hasgeek.com',
+  userPhone: '7676332020',
+  cardNo: '',
+  cardExpiry: '',
+  cardCvv: '',
+  //URL for Ticket booking
+  url: 'http://boxoffice.travis.dev:6500',
+  //URL for Free ticket booking
+  couponUrl: 'http://boxoffice.travis.dev:6500?code=coupon2',
+  //URL for Discounted tickets booking
+  couponCodeUrl: 'http://boxoffice.travis.dev:6500?code=coupon1'
+};
