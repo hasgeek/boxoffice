85e4f79b3dd8db6e4d2b36e1772694a0f4286fc8
Shreyas Satish <shreyas.satish@gmail.com>
2016-09-22 17:06:35 +0530

    added tickets report

--- boxoffice/views/admin_report.py
+++ boxoffice/views/admin_report.py
@@ -3,7 +3,7 @@
 from flask import Response, jsonify
 from .. import app, lastuser
 from coaster.views import load_models, render_with
-from boxoffice.models import db, ItemCollection, Order, Item, LineItem, Assignee, LINE_ITEM_STATUS
+from boxoffice.models import db, ItemCollection, Order, Item, LineItem, Assignee, DiscountPolicy, LINE_ITEM_STATUS
 
 
 def jsonify_report(data_dict):
@@ -21,6 +21,11 @@ def admin_report(item_collection):
     return dict(title=item_collection.organization.title, item_collection=item_collection)
 
 
+def generate_csv(headers, rows):
+    for row in headers + rows:
+        yield ','.join([unicode(attr) for attr in row]) + '\n'
+
+
 @app.route('/admin/ic/<ic_id>/reports/attendee.csv')
 @lastuser.requires_login
 @load_models(
@@ -30,10 +35,22 @@ def admin_report(item_collection):
 def admin_assignee_report(item_collection):
     line_item_join = db.outerjoin(LineItem, Assignee).join(Item).join(Order)
     line_item_stmt = db.select([LineItem.id, Item.title, Order.buyer_fullname, Order.buyer_email, Order.buyer_phone, Assignee.fullname, Assignee.email, Assignee.phone, Assignee.details]).select_from(line_item_join).where(LineItem.status == LINE_ITEM_STATUS.CONFIRMED).where(Assignee.current == True).where(Order.item_collection == item_collection).order_by('created_at')
-    records = db.session.execute(line_item_stmt).fetchall()
+    rows = db.session.execute(line_item_stmt).fetchall()
     headers = [['line item id', 'item title', 'buyer fullname', 'buyer email', 'buyer phone', 'attendee fullname', 'attendee email', 'attendee phone', 'attendee details']]
 
-    def generate():
-        for row in headers + records:
-            yield ','.join([unicode(attr) for attr in row]) + '\n'
-    return Response(generate(), mimetype='text/csv')
+    return Response(generate_csv(headers, rows), mimetype='text/csv')
+
+
+@app.route('/admin/ic/<ic_id>/reports/tickets.csv')
+@lastuser.requires_login
+@load_models(
+    (ItemCollection, {'id': 'ic_id'}, 'item_collection'),
+    permission='org_admin'
+    )
+def admin_line_items_report(item_collection):
+    line_item_join = db.outerjoin(LineItem, Assignee).outerjoin(DiscountPolicy).join(Item).join(Order)
+    line_item_stmt = db.select([LineItem.id, Item.title, LineItem.base_amount, LineItem.discounted_amount, LineItem.final_amount, DiscountPolicy.title, Order.buyer_fullname, Order.buyer_email, Order.buyer_phone, Assignee.fullname, Assignee.email, Assignee.phone, Assignee.details]).select_from(line_item_join).where(LineItem.status == LINE_ITEM_STATUS.CONFIRMED).where(Assignee.current == True).where(Order.item_collection == item_collection).order_by('created_at')
+    rows = db.session.execute(line_item_stmt).fetchall()
+    headers = [['line item id', 'item title', 'base amount', 'discounted amount', 'final amount', 'discount policy', 'buyer fullname', 'buyer email', 'buyer phone', 'attendee fullname', 'attendee email', 'attendee phone', 'attendee details']]
+
+    return Response(generate_csv(headers, rows), mimetype='text/csv')
