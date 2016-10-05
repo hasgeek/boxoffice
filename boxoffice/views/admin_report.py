d0215a57823d6093912634e9bf77f3fa1faf0f8e
Shreyas Satish <shreyas.satish@gmail.com>
2016-10-05 23:23:32 +0530

    using id instead of ic_id

--- boxoffice/views/admin_report.py
+++ boxoffice/views/admin_report.py
@@ -10,12 +10,11 @@ def jsonify_report(data_dict):
     return jsonify(org_name=data_dict['item_collection'].organization.name, title=data_dict['item_collection'].title)
 
 
-@app.route('/admin/ic/<ic_id>/reports')
+@app.route('/admin/ic/<id>/reports')
 @lastuser.requires_login
 @load_models(
-    (ItemCollection, {'id': 'ic_id'}, 'item_collection'),
-    permission='org_admin'
-    )
+    (ItemCollection, {'id': 'id'}, 'item_collection'),
+    permission='org_admin')
 @render_with({'text/html': 'index.html', 'application/json': jsonify_report}, json=True)
 def admin_report(item_collection):
     return dict(title=item_collection.organization.title, item_collection=item_collection)
@@ -26,12 +25,11 @@ def generate_csv(headers, rows):
         yield ','.join([unicode(attr) for attr in row]) + '\n'
 
 
-@app.route('/admin/ic/<ic_id>/reports/tickets.csv')
+@app.route('/admin/ic/<id>/reports/tickets.csv')
 @lastuser.requires_login
 @load_models(
-    (ItemCollection, {'id': 'ic_id'}, 'item_collection'),
-    permission='org_admin'
-    )
+    (ItemCollection, {'id': 'id'}, 'item_collection'),
+    permission='org_admin')
 def admin_line_items_report(item_collection):
     line_item_join = db.outerjoin(LineItem, Assignee).outerjoin(DiscountPolicy).join(Item).join(Order)
     line_item_query = db.select([LineItem.id, Item.title, LineItem.base_amount, LineItem.discounted_amount, LineItem.final_amount, DiscountPolicy.title, Order.buyer_fullname, Order.buyer_email, Order.buyer_phone, Assignee.fullname, Assignee.email, Assignee.phone, Assignee.details])\
