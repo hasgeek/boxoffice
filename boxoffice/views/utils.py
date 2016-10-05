84ca0e42af73a1a40e58248d8c815cd38700ee6d
Shreyas Satish <shreyas.satish@gmail.com>
2016-10-06 00:54:46 +0530

    value is already utf-8

--- boxoffice/views/utils.py
+++ boxoffice/views/utils.py
@@ -82,4 +82,4 @@ def csv_response(headers, rows):
     csv_writer.writerow(headers)
     for row in rows:
         csv_writer.writerow(row)
-    return Response(unicode(stream.getvalue()), mimetype='text/csv')
+    return Response(stream.getvalue(), mimetype='text/csv')
