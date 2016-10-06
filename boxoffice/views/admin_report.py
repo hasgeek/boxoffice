73c970968d211f79b120254524844ae554c744cd
Shreyas Satish <shreyas.satish@gmail.com>
2016-10-06 14:26:54 +0530

    moved query into a method in LineItem

--- boxoffice/views/admin_report.py
+++ boxoffice/views/admin_report.py
@@ -27,9 +27,6 @@ def admin_report(item_collection):
     (ItemCollection, {'id': 'id'}, 'item_collection'),
     permission='org_admin')
 def admin_line_items_report(item_collection):
-    line_item_join = db.outerjoin(LineItem, Assignee).outerjoin(DiscountPolicy).join(Item).join(Order)
-    line_item_query = db.select([LineItem.id, Item.title, LineItem.base_amount, LineItem.discounted_amount, LineItem.final_amount, DiscountPolicy.title, Order.buyer_fullname, Order.buyer_email, Order.buyer_phone, Assignee.fullname, Assignee.email, Assignee.phone, Assignee.details]).select_from(line_item_join).where(LineItem.status == LINE_ITEM_STATUS.CONFIRMED).where(Assignee.current == True).where(Order.item_collection == item_collection).order_by('created_at')
-    rows = db.session.execute(line_item_query).fetchall()
     headers = ['ticket id', 'item title', 'base amount', 'discounted amount', 'final amount', 'discount policy', 'buyer fullname', 'buyer email', 'buyer phone', 'attendee fullname', 'attendee email', 'attendee phone', 'attendee details']
-
+    rows = LineItem.fetch_all_details(item_collection)
     return csv_response(headers, rows)
