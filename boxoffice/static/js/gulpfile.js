41fb98aa5226cca6e69e4ee84223afbf63259503
Shreyas Satish <shreyas.satish@gmail.com>
2016-10-25 14:34:26 +0530

    added uglify to gulp pipeline

--- boxoffice/static/js/gulpfile.js
+++ boxoffice/static/js/gulpfile.js
@@ -4,6 +4,8 @@ var browserify = require('browserify');
 var watchify = require('watchify');
 var babelify = require('babelify');
 var source = require('vinyl-source-stream');
+var uglify = require('gulp-uglify');
+var streamify = require('gulp-streamify');
 var _ = require('underscore');
 
 var config = {
@@ -26,6 +28,7 @@ function bundle() {
     .bundle()
     .on('error', function(err) { console.log('Error: ' + err.message); })
     .pipe(source(config.outputFile))
+    .pipe(streamify(uglify()))
     .pipe(gulp.dest(config.outputDir));
 }
 
