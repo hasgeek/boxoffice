44fba4a7ae54782bf79046ce95c0f44975714e98
Vidya Ramakrishnan <vidya@hasgeek.in>
2016-11-02 13:29:28 +0530

    Add Organization details field to siteadmin

--- boxoffice/siteadmin.py
+++ boxoffice/siteadmin.py
@@ -14,6 +14,7 @@ class OrganizationModelView(SiteAdminModelView):
     column_list = ('id', 'title')
     form_list = ('id', 'userid', 'title')
     form_excluded_columns = ['userid', 'item_collections', 'discount_policies', 'orders', 'created_at', 'updated_at']
+    form_overrides = dict(details=JSONField)
 
 
 class ItemCollectionModelView(SiteAdminModelView):
