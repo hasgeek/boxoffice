9ea18b7b80b180138312396c67d47866d4165e43
Shreyas Satish <shreyas.satish@gmail.com>
2016-12-20 17:47:32 +0530

    added a row handler to csv_response, and purchase date to the report (#123)
    * added a row handler to csv_response, and purchase date to the report
    
    * added comment
    
    * use user locale if present for datetime formatting
    
    * use writerows
    
    * use generator
    
    * remove unnecessary default
    
    * wrap line for readability

--- boxoffice/views/admin_report.py
+++ boxoffice/views/admin_report.py
@@ -3,9 +3,10 @@
 from flask import jsonify
 from .. import app, lastuser
 from coaster.views import load_models, render_with
+from baseframe import localize_timezone, get_locale
 from boxoffice.models import ItemCollection, LineItem
 from boxoffice.views.utils import csv_response
-
+from babel.dates import format_datetime
 
 def jsonify_report(data_dict):
     return jsonify(org_name=data_dict['item_collection'].organization.name, item_collection_title=data_dict['item_collection'].title)
@@ -27,6 +28,13 @@ def admin_report(item_collection):
     (ItemCollection, {'id': 'id'}, 'item_collection'),
     permission='org_admin')
 def tickets_report(item_collection):
-    headers = ['ticket id', 'invoice no', 'ticket type', 'base amount', 'discounted amount', 'final amount', 'discount policy', 'discount code', 'buyer fullname', 'buyer email', 'buyer phone', 'attendee fullname', 'attendee email', 'attendee phone', 'attendee details', 'utm_campaign', 'utm_source', 'utm_medium', 'utm_term', 'utm_content', 'utm_id', 'gclid', 'referrer']
+    headers = ['ticket id', 'invoice no', 'ticket type', 'base amount', 'discounted amount', 'final amount', 'discount policy', 'discount code', 'buyer fullname', 'buyer email', 'buyer phone', 'attendee fullname', 'attendee email', 'attendee phone', 'attendee details', 'utm_campaign', 'utm_source', 'utm_medium', 'utm_term', 'utm_content', 'utm_id', 'gclid', 'referrer', 'date']
     rows = LineItem.fetch_all_details(item_collection)
-    return csv_response(headers, rows)
+
+    def row_handler(row):
+        row_list = list(row)
+        # localize datetime
+        row_list[-1] = format_datetime(localize_timezone(row_list[-1]), format='long', locale=get_locale())
+        return row_list
+
+    return csv_response(headers, rows, row_handler=row_handler)
