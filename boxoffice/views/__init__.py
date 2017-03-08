a4a580b573bf0125b167ed8b660c2713dab25465
Shreyas Satish <shreyas.satish@gmail.com>
2017-03-09 04:38:15 +0530

    move admin item to new file

--- boxoffice/views/__init__.py
+++ boxoffice/views/__init__.py
@@ -1,3 +1,3 @@
 # -*- coding: utf-8 -*-
 
-from . import admin, item_collection, order, participant, login, admin_item_collection, admin_order, admin_discount, admin_report
+from . import admin, item_collection, order, participant, login, admin_item_collection, admin_order, admin_discount, admin_report, admin_item
---
+++ boxoffice/views/admin_item.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+from .. import app, lastuser
+from coaster.views import load_models, requestargs
+from boxoffice.models import db, Organization, ItemCollection, Item
+from boxoffice.views.utils import api_error, api_success
+from utils import xhr_only
+
+
+@app.route('/admin/o/<org>/items')
+@lastuser.requires_login
+@xhr_only
+@load_models(
+    (Organization, {'name': 'org'}, 'organization'),
+    permission='org_admin'
+    )
+@requestargs('search')
+def items(organization, search=None):
+    if search:
+        filtered_items = Item.query.join(ItemCollection).filter(
+            ItemCollection.organization == organization).filter(
+            Item.title.ilike('%{query}%'.format(query=search))).options(db.load_only('id')).all()
+        return api_success(result={'items': [{'id': str(item.id), 'title': item.title}
+            for item in filtered_items]}, doc="Filtered items", status_code=200)
+    else:
+        return api_error(message='Missing search query', status_code=400)
