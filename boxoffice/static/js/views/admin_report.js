81a04ce8e90412cb8c7ba5194f346bd051916388
Vidya Ramakrishnan <vidya@hasgeek.in>
2017-03-15 19:07:33 +0530

    Add default value and define reportType.

--- boxoffice/static/js/views/admin_report.js
+++ boxoffice/static/js/views/admin_report.js
@@ -9,24 +9,26 @@ export const ReportView = {
       url: urlFor('index', {resource: 'reports', scope_ns: 'ic', scope_id: ic_id, root: true})
     }).done(({org_name, title}) => {
       // Initial render
-      let main_ractive = new Ractive({
+      let reportsComponent = new Ractive({
         el: '#main-content-area',
         template: ReportTemplate,
         data:  {
           title: title,
-          reports_url: function() {
-            let report_type = this.get('report_type');
+          // default selected value of report-type
+          reportType: "tickets",
+          reportsUrl: function() {
+            let reportType = this.get('reportType');
             return urlFor('index', {
-              resource: report_type,
+              resource: reportType,
               scope_ns: 'ic',
               scope_id: ic_id,
               ext: 'csv',
               root: true
             });
           },
-          reports_filename: function() {
+          reportsFilename: function() {
             let filename = this.get('title').replace(/ /g, '_');
-            return filename + '_' + this.get('report_type') + '.csv';
+            return filename + '_' + this.get('reportType') + '.csv';
           }
         }
       });
