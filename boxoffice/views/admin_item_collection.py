30c24781f8d0d5e9641ec4dd0ef026af6f8bb254
Shreyas Satish <shreyas.satish@gmail.com>
2017-04-24 17:11:44 +0530

    use isoformat for today sales

--- boxoffice/views/admin_item_collection.py
+++ boxoffice/views/admin_item_collection.py
@@ -53,7 +53,7 @@ def admin_item_collection(item_collection):
     for sales_date, sales_count in counts_per_date_per_item(item_collection, g.user.timezone).items():
         date_sales[sales_date.isoformat()] = sales_by_date(sales_date, item_ids, g.user.timezone)
         date_item_counts[sales_date.isoformat()] = sales_count
-    today_sales = date_sales.get(localize_timezone(datetime.datetime.utcnow(), g.user.timezone).date(), Decimal(0))
+    today_sales = date_sales.get(localize_timezone(datetime.datetime.utcnow(), g.user.timezone).date().isoformat(), Decimal(0))
     return dict(title=item_collection.title, item_collection=item_collection, date_item_counts=date_item_counts,
         date_sales=date_sales, today_sales=today_sales,
         sales_delta=sales_delta(g.user.timezone, item_ids))
