3582c40fae38097d65b12cd870ee2cd0fa73b754
Karthik Balakrishnan <karthikb351@gmail.com>
2017-06-05 14:29:30 +0530

    API route for order details

--- boxoffice/views/admin_report.py
+++ boxoffice/views/admin_report.py
@@ -4,7 +4,7 @@ from flask import jsonify
 from .. import app, lastuser
 from coaster.views import load_models, render_with
 from baseframe import localize_timezone, get_locale
-from boxoffice.models import ItemCollection, LineItem
+from boxoffice.models import Organization, ItemCollection, LineItem
 from boxoffice.views.utils import csv_response
 from babel.dates import format_datetime
 
@@ -55,6 +55,32 @@ def attendees_report(item_collection):
             if detail not in headers:
                 headers.append(detail)
     rows = item_collection.fetch_assignee_details
+
+    def row_handler(row):
+        # Convert row to a dict
+        # row[-1] contains attendee details which is already a dict
+        dict_row = dict(zip(headers, row[:-1]))
+        dict_row.update(row[-1])
+        return dict_row
+
+    return csv_response(headers, rows, row_type='dict', row_handler=row_handler)
+
+
+@app.route('/api/1/organization/<org>/ic/<ic_id>/orders.csv')
+@load_models(
+    (Organization, {'name': 'org'}, 'organization'),
+    (ItemCollection, {'id': 'ic_id'}, 'item_collection'),
+    permission='org_admin')
+def orders_api(organization, item_collection):
+    check_api_access(organization.details.get('access_token'))
+    headers = ['ticket_id', 'order_id', 'receipt_no', 'ticket_type', 'base_amount', 'discounted_amount', 'final_amount', 'discount_policy', 'discount_code', 'buyer_fullname', 'buyer_email', 'buyer_phone', 'attendee_fullname', 'attendee_email', 'attendee_phone', 'utm_campaign', 'utm_source', 'utm_medium', 'utm_term', 'utm_content', 'utm_id', 'gclid', 'referrer', 'date']
+
+    for item in item_collection.items:
+        for detail in item.assignee_details.keys():
+            if detail not in headers:
+                headers.append(detail)
+
+    rows = item_collection.fetch_all_details
 
     def row_handler(row):
         # Convert row to a dict
