63ebd065a03aa021cc1157273b67e83252764a40
Karthik Balakrishnan <karthikb351@gmail.com>
2017-06-09 17:42:02 +0530

    Access Token based API route for order details (CSV) (#159) - Fixes #156
    * API route for order details
    
    * Sanity check for assignee_details and refactoring headers
    
    * Localise date in orders api
    
    * Remove attendee_details from csv header
    
    * Cleaner way to remove value from csv_headers
    
    * Use namedtuples, check if org owns item collection
    
    * Prefex attendee_detail columns is csv to avoid conflict
    
    * Generalise datetime formatting
    
    * Additional comments, spelling correction

--- boxoffice/models/line_item.py
+++ boxoffice/models/line_item.py
@@ -23,11 +23,13 @@ class LINE_ITEM_STATUS(LabeledEnum):
     VOID = (3, __("Void"))
 
 
-LineItemTup = namedtuple('LineItem', ['item_id', 'id', 'base_amount', 'discount_policy_id', 'discount_coupon_id', 'discounted_amount', 'final_amount'])
+LineItemTuple = namedtuple('LineItemTuple', ['item_id', 'id', 'base_amount', 'discount_policy_id', 'discount_coupon_id', 'discounted_amount', 'final_amount'])
+
+HeadersAndDataTuple = namedtuple('HeadersAndDataTuple', ['headers', 'data'])
 
 
 def make_ntuple(item_id, base_amount, **kwargs):
-    return LineItemTup(item_id,
+    return LineItemTuple(item_id,
         kwargs.get('line_item_id', None),
         base_amount,
         kwargs.get('discount_policy_id', None),
@@ -176,6 +178,7 @@ def fetch_all_details(self):
     """
     Returns details for all the line items in a given item collection, along with the associated
     assignee (if any), discount policy (if any), discount coupon (if any), item, order and order session (if any)
+    as a tuple of (keys, rows)
     """
     line_item_join = db.outerjoin(LineItem, Assignee, db.and_(LineItem.id == Assignee.line_item_id,
         Assignee.current == True)).outerjoin(DiscountCoupon,
@@ -190,16 +193,19 @@ def fetch_all_details(self):
         Order.paid_at]).select_from(line_item_join).where(LineItem.status ==
         LINE_ITEM_STATUS.CONFIRMED).where(Order.item_collection ==
         self).order_by(LineItem.ordered_at)
-    return db.session.execute(line_item_query).fetchall()
+    return HeadersAndDataTuple(
+        ['ticket_id', 'order_id', 'receipt_no', 'ticket_type', 'base_amount', 'discounted_amount', 'final_amount', 'discount_policy', 'discount_code', 'buyer_fullname', 'buyer_email', 'buyer_phone', 'attendee_fullname', 'attendee_email', 'attendee_phone', 'attendee_details', 'utm_campaign', 'utm_source', 'utm_medium', 'utm_term', 'utm_content', 'utm_id', 'gclid', 'referrer', 'date'],
+        db.session.execute(line_item_query).fetchall()
+        )
 
 
-ItemCollection.fetch_all_details = property(fetch_all_details)
+ItemCollection.fetch_all_details = fetch_all_details
 
 
 def fetch_assignee_details(self):
     """
     Returns invoice_no, ticket title, assignee fullname, assignee email, assignee phone and assignee details
-    for all the line items in a given item collection
+    for all the line items in a given item collection as a tuple of (keys, rows)
     """
     line_item_join = db.join(LineItem, Assignee, db.and_(LineItem.id == Assignee.line_item_id,
         Assignee.current == True)).join(Item).join(Order)
@@ -207,10 +213,13 @@ def fetch_assignee_details(self):
         Assignee.email, Assignee.phone, Assignee.details]).select_from(line_item_join).where(LineItem.status ==
         LINE_ITEM_STATUS.CONFIRMED).where(Order.item_collection ==
         self).order_by(LineItem.ordered_at)
-    return db.session.execute(line_item_query).fetchall()
+    return HeadersAndDataTuple(
+        ['receipt_no', 'ticket_no', 'ticket_id', 'ticket_type', 'attendee_fullname', 'attendee_email', 'attendee_phone', 'attendee_details'],
+        db.session.execute(line_item_query).fetchall()
+        )
 
 
-ItemCollection.fetch_assignee_details = property(fetch_assignee_details)
+ItemCollection.fetch_assignee_details = fetch_assignee_details
 
 
 def get_availability(cls, item_ids):
