3a79d49c15b785b408bff1b93a09c7a6f98a8ff7
Shreyas Satish <shreyas.satish@gmail.com>
2017-06-12 14:11:20 +0530

    adding roles to item collection

--- boxoffice/models/item_collection.py
+++ boxoffice/models/item_collection.py
@@ -18,3 +18,18 @@ class ItemCollection(BaseScopedNameMixin, db.Model):
     organization_id = db.Column(db.Integer, db.ForeignKey('organization.id'), nullable=False)
     organization = db.relationship(Organization, backref=db.backref('item_collections', cascade='all, delete-orphan'))
     parent = db.synonym('organization')
+
+    __roles__ = {
+        'description': {
+            'write': {'item_collection_owner'},
+            'read': {'item_collection_owner'}
+        }
+    }
+
+    def roles(self, user=None, inherited=None, token=None):
+        roles = super(ItemCollection, self).roles(user, inherited, token)
+        if user or token:
+            roles.add('user')
+        if user:
+            roles.add('item_collection_owner')
+        return roles
