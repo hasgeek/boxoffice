fb7e9526257b34805d7d6c5988a725c1f9a87ead
Shreyas Satish <shreyas.satish@gmail.com>
2017-07-04 16:49:34 +0530

    added more attributes to Invoice, and added InvoiceLineItem

--- boxoffice/models/invoice.py
+++ boxoffice/models/invoice.py
@@ -1,23 +1,60 @@
 # -*- coding: utf-8 -*-
 
-from boxoffice.models import db, BaseMixin, Order, PaymentTransaction
+from decimal import Decimal
+from boxoffice.models import db, BaseMixin
+from baseframe import __
+from coaster.utils import LabeledEnum
+
+__all__ = ['Invoice', 'InvoiceLineItem']
 
 
-__all__ = ['Invoice']
+class ITEM_TYPE(LabeledEnum):
+    GOOD = (0, __("Good"))
+    SERVICE = (1, __("Service"))
 
 
 class Invoice(BaseMixin, db.Model):
     __tablename__ = 'invoice'
     __uuid_primary_key__ = True
+    __table_args__ = (db.UniqueConstraint('organization_id', 'invoice_no'),)
 
     invoicee_name = db.Column(db.Unicode(255), nullable=True)
+    invoicee_email = db.Column(db.Unicode(254), nullable=True)
+    invoice_no = db.Column(db.Unicode(32), nullable=True)
+    invoiced_at = db.Column(db.DateTime, nullable=True)
     street_address = db.Column(db.Unicode(255), nullable=True)
     city = db.Column(db.Unicode(255), nullable=True)
     state = db.Column(db.Unicode(255), nullable=True)
     country = db.Column(db.Unicode(255), nullable=True)
-    postcode = db.Column(db.Unicode(255), nullable=True)
+    postcode = db.Column(db.Unicode(8), nullable=True)
     # GSTIN in the case of India
     taxid = db.Column(db.Unicode(255), nullable=True)
+    # India specific: this is the state short code. Eg: KA for Karnataka
+    place_of_supply = db.Column(db.Unicode(3), nullable=True)
 
     customer_order_id = db.Column(None, db.ForeignKey('customer_order.id'), nullable=False, index=True, unique=False)
-    order = db.relationship(Order, backref=db.backref('invoice', cascade='all, delete-orphan'))
+    order = db.relationship('Order', backref=db.backref('invoices', cascade='all, delete-orphan'))
+
+    organization_id = db.Column(None, db.ForeignKey('organization.id'), nullable=False)
+    organization = db.relationship('Organization', backref=db.backref('invoices', cascade='all, delete-orphan', lazy='dynamic'))
+
+
+class InvoiceLineItem(BaseMixin, db.Model):
+    __tablename__ = 'invoice_line_item'
+    __uuid_primary_key__ = True
+    __table_args__ = (db.UniqueConstraint('invoice_id', 'seq'),)
+
+    seq = db.Column(db.Integer, nullable=False)
+    item_title = db.Column(db.Unicode(255), nullable=False)
+    # In India, this will be GST
+    tax_type = db.Column(db.Unicode(255), nullable=False)
+    item_type = db.Column(db.Integer, default=ITEM_TYPE.SERVICE, nullable=False)
+    discount_title = db.Column(db.Unicode(255), nullable=False)
+
+    currency = db.Column(db.Unicode(3), nullable=False)
+    base_amount = db.Column(db.Numeric, default=Decimal(0), nullable=False)
+    discounted_amount = db.Column(db.Numeric, default=Decimal(0), nullable=False)
+    final_amount = db.Column(db.Numeric, default=Decimal(0), nullable=False)
+
+    invoice_id = db.Column(None, db.ForeignKey('invoice.id'), nullable=False, index=True, unique=False)
+    invoice = db.relationship(Invoice, backref=db.backref('line_items', cascade='all, delete-orphan'))
---
+++ migrations/versions/ed34298e43ec_create_invoice.py
@@ -0,0 +1,70 @@
+"""create_invoice
+
+Revision ID: ed34298e43ec
+Revises: 36f458047cfd
+Create Date: 2017-07-04 16:10:58.493923
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = 'ed34298e43ec'
+down_revision = '36f458047cfd'
+
+from alembic import op
+import sqlalchemy as sa
+from sqlalchemy.dialects import postgresql
+import sqlalchemy_utils
+
+
+def upgrade():
+    op.create_table('invoice',
+        sa.Column('created_at', sa.DateTime(), nullable=False),
+        sa.Column('updated_at', sa.DateTime(), nullable=False),
+        sa.Column('invoicee_name', sa.Unicode(length=255), nullable=True),
+        sa.Column('invoicee_email', sa.Unicode(length=254), nullable=True),
+        sa.Column('invoice_no', sa.Unicode(length=32), nullable=True),
+        sa.Column('invoiced_at', sa.DateTime(), nullable=True),
+        sa.Column('street_address', sa.Unicode(length=255), nullable=True),
+        sa.Column('city', sa.Unicode(length=255), nullable=True),
+        sa.Column('state', sa.Unicode(length=255), nullable=True),
+        sa.Column('country', sa.Unicode(length=255), nullable=True),
+        sa.Column('postcode', sa.Unicode(length=8), nullable=True),
+        sa.Column('taxid', sa.Unicode(length=255), nullable=True),
+        sa.Column('place_of_supply', sa.Unicode(length=3), nullable=True),
+        sa.Column('customer_order_id', sqlalchemy_utils.types.uuid.UUIDType(binary=False), nullable=False),
+        sa.Column('organization_id', sa.Integer(), nullable=False),
+        sa.Column('id', sqlalchemy_utils.types.uuid.UUIDType(binary=False), nullable=False),
+        sa.ForeignKeyConstraint(['customer_order_id'], ['customer_order.id'], ),
+        sa.ForeignKeyConstraint(['organization_id'], ['organization.id'], ),
+        sa.PrimaryKeyConstraint('id'),
+        sa.UniqueConstraint('organization_id', 'invoice_no')
+    )
+    op.create_index(op.f('ix_invoice_customer_order_id'), 'invoice', ['customer_order_id'], unique=False)
+    op.create_table('invoice_line_item',
+        sa.Column('created_at', sa.DateTime(), nullable=False),
+        sa.Column('updated_at', sa.DateTime(), nullable=False),
+        sa.Column('seq', sa.Integer(), nullable=False),
+        sa.Column('item_title', sa.Unicode(length=255), nullable=False),
+        sa.Column('tax_type', sa.Unicode(length=255), nullable=False),
+        sa.Column('item_type', sa.Integer(), nullable=False),
+        sa.Column('discount_title', sa.Unicode(length=255), nullable=False),
+        sa.Column('currency', sa.Unicode(length=3), nullable=False),
+        sa.Column('base_amount', sa.Numeric(), nullable=False),
+        sa.Column('discounted_amount', sa.Numeric(), nullable=False),
+        sa.Column('final_amount', sa.Numeric(), nullable=False),
+        sa.Column('invoice_id', sqlalchemy_utils.types.uuid.UUIDType(binary=False), nullable=False),
+        sa.Column('id', sqlalchemy_utils.types.uuid.UUIDType(binary=False), nullable=False),
+        sa.ForeignKeyConstraint(['invoice_id'], ['invoice.id'], ),
+        sa.PrimaryKeyConstraint('id'),
+        sa.UniqueConstraint('invoice_id', 'seq')
+    )
+    op.create_index(op.f('ix_invoice_line_item_invoice_id'), 'invoice_line_item', ['invoice_id'], unique=False)
+    op.add_column(u'item_collection', sa.Column('tax_type', sa.Unicode(length=80), nullable=True))
+
+
+def downgrade():
+    op.drop_column(u'item_collection', 'tax_type')
+    op.drop_index(op.f('ix_invoice_line_item_invoice_id'), table_name='invoice_line_item')
+    op.drop_table('invoice_line_item')
+    op.drop_index(op.f('ix_invoice_customer_order_id'), table_name='invoice')
+    op.drop_table('invoice')
