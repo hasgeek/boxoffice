98fd6f300f2e9b3c77d45b2192a4d6e693f46676
Shreyas Satish <shreyas.satish@gmail.com>
2018-04-13 17:59:42 +0530

    search items with item collection title or item title

--- boxoffice/views/admin_item.py
+++ boxoffice/views/admin_item.py
@@ -23,10 +23,12 @@ from utils import xhr_only
 def items(organization, search=None):
     if search:
         filtered_items = db.session.query(Item, ItemCollection).filter(
-            ItemCollection.organization == organization).filter(
-            Item.title.ilike('%{query}%'.format(query=search))).join(Item.item_collection).options(
-            db.Load(Item).load_only('id', 'title'),
-            db.Load(ItemCollection).load_only('id', 'title')).all()
+            ItemCollection.organization == organization, db.or_(
+                Item.title.ilike('%{query}%'.format(query=search)),
+                ItemCollection.title.ilike('%{query}%'.format(query=search)))).join(
+            Item.item_collection).options(
+                db.Load(Item).load_only('id', 'title'),
+                db.Load(ItemCollection).load_only('id', 'title')).all()
         return api_success(result={'items': [{
             'id': str(item_tuple[0].id),
             'title': "{ic_title}: {title}".format(ic_title=item_tuple[1].title, title=item_tuple[0].title)
