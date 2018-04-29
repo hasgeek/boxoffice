b01d0f8b5eca8773a672043063b765a84d3af634
Shreyas Satish <shreyas.satish@gmail.com>
2018-04-29 16:19:46 +0530

    fixed email validation and access_token generation

--- boxoffice/forms/org.py
+++ boxoffice/forms/org.py
@@ -10,24 +10,29 @@ from .utils import validate_json, format_json
 __all__ = ['OrgForm', 'NewOrgForm']
 
 
-DEFAULT_ORG_DETAILS = {
-    u'access_token': buid(),
-    u'address': u'',
-    u'cin': u'',
-    u'logo': u'',
-    u'pan': u'',
-    u'refund_policy': u'',
-    u'support_email': u'',
-    u'ticket_faq': u'',
-    u'website': u''
-}
+def get_default_details():
+    return {
+        u'access_token': buid(),
+        u'address': u'',
+        u'cin': u'',
+        u'logo': u'',
+        u'pan': u'',
+        u'refund_policy': u'',
+        u'support_email': u'',
+        u'ticket_faq': u'',
+        u'website': u''
+    }
 
 
 class OrgForm(forms.Form):
     contact_email = forms.EmailField(__("Contact email"),
-        validators=[forms.validators.DataRequired(__("Please enter an email address")), forms.validators.Length(min=5, max=80)])
+        validators=[
+            forms.validators.DataRequired(__("We need a valid email address")),
+            forms.validators.Length(min=5, max=80, message=__("%%(max)d characters maximum")),
+            forms.validators.ValidEmail(__("This does not appear to be a valid email address"))],
+        filters=[forms.filters.strip()])
     details = forms.TextAreaField(__("Details"), filters=[format_json],
-        validators=[validate_json], default=DEFAULT_ORG_DETAILS)
+        validators=[validate_json], default=get_default_details)
     invoicer = QuerySelectField(__("Parent organization"), get_label='title',
         validators=[forms.validators.DataRequired(__("Please select a parent organization"))])
 
