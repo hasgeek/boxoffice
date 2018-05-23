e4313fafc3feabac0e6a931fb3f89cf45cd91819
Bibhas <me@bibhas.in>
2018-05-23 17:23:03 +0530

    no longer checking for request method inside org form filter

--- boxoffice/forms/utils.py
+++ boxoffice/forms/utils.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import json
-from flask import request
+import six
 import baseframe.forms as forms
 from baseframe import __
 
@@ -14,10 +14,11 @@ def validate_json(form, field):
 
 
 def format_json(data):
-    if request.method == 'GET':
-        return json.dumps(data, indent=4, sort_keys=True)
     # `json.loads` doesn't raise an exception for "null"
     # so assign a default value of `{}`
     if not data or data == 'null':
-        return json.dumps({})
-    return data
+        data = {}
+    elif isinstance(data, six.string_types):
+        # This happens when putting JSON data from the form into the database
+        data = json.loads(data)
+    return json.dumps(data, indent=4, sort_keys=True)
