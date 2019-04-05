be0f94d8bdb786371ef84ef920ebf48981030849
Bibhas <me@bibhas.in>
2019-04-05 18:25:13 +0530

    providing blank first option

--- boxoffice/forms/item.py
+++ boxoffice/forms/item.py
@@ -77,13 +77,13 @@ class ItemForm(forms.Form):
     event_date = forms.DateField(__("Event date"), validators=[forms.validators.DataRequired(__("Please specify a date for the event"))])
     cancellable_until = forms.DateTimeField(__("Cancellable until"), validators=[forms.validators.Optional()])
     place_supply_state_code = forms.SelectField(__("State"),
-        choices=[(state['short_code'], state['name']) for state in sorted(indian_states, key=lambda k: k['name'])],
-        description=__("Place of supply"), coerce=int,
-        default=indian_states_dict['KA']['short_code'])
+        choices=[('', '')] + [(state['short_code'], state['name']) for state in sorted(indian_states, key=lambda k: k['name'])],
+        description=__("Place of supply"), coerce=int, default=indian_states_dict['KA']['short_code'],
+        validators=[forms.validators.DataRequired(__("Please select a state"))])
     place_supply_country_code = forms.SelectField(__("Country"),
-        choices=[(country.alpha_2, country.name) for country in sorted(pycountry.countries, key=lambda k: k.name)],
-        description=__("Place of supply"),
-        default='')
+        choices=[('', '')] + [(country.alpha_2, country.name) for country in sorted(pycountry.countries, key=lambda k: k.name)],
+        description=__("Place of supply"), default='IN',
+        validators=[forms.validators.DataRequired(__("Please select a country"))])
 
     def set_queries(self):
         self.category.query = Category.query.join(ItemCollection).filter(
--- boxoffice/forms/item_collection.py
+++ boxoffice/forms/item_collection.py
@@ -2,7 +2,7 @@
 
 from pycountry import pycountry
 from baseframe import __
-import baseframe.forms as forms
+from baseframe import forms
 from boxoffice.data import indian_states, indian_states_dict
 
 __all__ = ['ItemCollectionForm']
@@ -12,13 +12,14 @@ class ItemCollectionForm(forms.Form):
     title = forms.StringField(__("Item Collection title"),
         validators=[forms.validators.DataRequired(__("Please specify a title")),
         forms.validators.Length(max=250)], filters=[forms.filters.strip()])
-    description_html = forms.TinyMce4Field(__("Description"))
+    description_html = forms.TinyMce4Field(__("Description"),
+        validators=[forms.validators.DataRequired(__("Please specify a description"))])
     tax_type = forms.SelectField(__("Tax type"), choices=[('GST', 'GST')], default='GST')
     place_supply_state_code = forms.SelectField(__("State"),
-        choices=[(state['short_code'], state['name']) for state in sorted(indian_states, key=lambda k: k['name'])],
-        description=__("Place of supply"), coerce=int,
-        default=indian_states_dict['KA']['short_code'])
+        choices=[('', '')] + [(state['short_code'], state['name']) for state in sorted(indian_states, key=lambda k: k['name'])],
+        description=__("Place of supply"), coerce=int, default=indian_states_dict['KA']['short_code'],
+        validators=[forms.validators.DataRequired(__("Please select a state"))])
     place_supply_country_code = forms.SelectField(__("Country"),
-        choices=[(country.alpha_2, country.name) for country in sorted(pycountry.countries, key=lambda k: k.name)],
-        description=__("Place of supply"),
-        default='IN')
+        choices=[('', '')] + [(country.alpha_2, country.name) for country in sorted(pycountry.countries, key=lambda k: k.name)],
+        description=__("Place of supply"), default='IN',
+        validators=[forms.validators.DataRequired(__("Please select a country"))])
