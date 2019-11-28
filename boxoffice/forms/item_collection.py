8b643c5c32b898e579978ee1de32b4e3334f38ac
Bibhas <github@bibhasdn.com>
2019-11-28 19:59:22 +0530

    Added 0 as value for empty choice in item collection form (#309)

--- boxoffice/forms/item_collection.py
+++ boxoffice/forms/item_collection.py
@@ -14,12 +14,18 @@ class ItemCollectionForm(forms.Form):
         validators=[forms.validators.DataRequired(__("Please specify a description"))])
     tax_type = forms.SelectField(__("Tax type"), choices=[('GST', 'GST')], default='GST')
     place_supply_state_code = forms.SelectField(__("State"),
-        choices=[('', '')] + [(state['short_code'], state['name']) for state in sorted(indian_states, key=lambda k: k['name'])],
-        description=__("Place of supply"), coerce=int, default=indian_states_dict['KA']['short_code'],
+        description=__("State of supply"), coerce=int, default=indian_states_dict['KA']['short_code'],
         validators=[forms.validators.DataRequired(__("Please select a state"))])
     place_supply_country_code = forms.SelectField(__("Country"),
-        description=__("Place of supply"), default='IN',
+        description=__("Country of supply"), default='IN',
         validators=[forms.validators.DataRequired(__("Please select a country"))])
 
     def set_queries(self):
+        self.place_supply_state_code.choices = [(0, '')] + [(state['short_code'], state['name']) for state in sorted(indian_states, key=lambda k: k['name'])]
         self.place_supply_country_code.choices = [('', '')] + localized_country_list()
+
+    def validate_place_supply_state_code(self, field):
+        if field.data <= 0:
+            # state short codes start from 1,
+            # and 0 means empty value as mentioned above in set_queries
+            raise forms.ValidationError(__("Please select a state"))
