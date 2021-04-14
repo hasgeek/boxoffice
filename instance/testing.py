db44d918ae92b64572728fa4954fd291c2f30731
Kiran Jonnalagadda <kiran@hasgeek.com>
2021-04-15 03:10:00 +0530

    Fix db engine name for SQLAlchemy 1.4

--- instance/testing.py
+++ instance/testing.py
@@ -1,7 +1,7 @@
 import os
 #: Database backend
 SECRET_KEY = 'testkey'
-SQLALCHEMY_DATABASE_URI = 'postgres:///boxoffice_testing'
+SQLALCHEMY_DATABASE_URI = 'postgresql:///boxoffice_testing'
 SERVER_NAME = 'boxoffice.travis.dev:6500'
 BASE_URL = 'http://' + SERVER_NAME
 
