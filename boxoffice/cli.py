3e07e568a6d99e5ef9d85f8bd41fa555a6e98670
Kiran Jonnalagadda <kiran@hasgeek.com>
2021-06-07 03:27:20 +0530

    Additional fixes from pre-commit (#367)

--- boxoffice/cli.py
+++ boxoffice/cli.py
@@ -3,10 +3,12 @@ from . import app
 
 @app.cli.command('dbconfig')
 def dbconfig():
-    """Show required database configuration"""
-    print('''
+    """Show required database configuration."""
+    print(  # NOQA: T001
+        '''
 -- Pipe this into psql as a super user. Example:
 -- flask dbconfig | sudo -u postgres psql boxoffice
 
 CREATE EXTENSION IF NOT EXISTS pg_trgm;
-''')
+'''
+    )
