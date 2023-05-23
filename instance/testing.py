e496c50af7b463c5b23f6a5b56da096ca82e942a
Kiran Jonnalagadda <kiran@hasgeek.com>
2023-05-23 17:29:34 +0530

    Use psycopg3 for tests

--- instance/testing.py
+++ instance/testing.py
@@ -2,19 +2,15 @@ import os
 
 #: Database backend
 SECRET_KEY = 'testkey'  # nosec
-SQLALCHEMY_DATABASE_URI = 'postgresql:///boxoffice_testing'  # nosec
-SERVER_NAME = 'boxoffice.travis.dev:6500'
+SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg:///boxoffice_testing'  # nosec
+SERVER_NAME = 'boxoffice.test:6500'
 BASE_URL = 'http://' + SERVER_NAME
 
 RAZORPAY_KEY_ID = os.environ.get('RAZORPAY_KEY_ID')
 RAZORPAY_KEY_SECRET = os.environ.get('RAZORPAY_KEY_SECRET')
 
-ALLOWED_ORIGINS = [
-    BASE_URL,
-    'http://boxoffice.travis.dev:6500/',
-    'http://shreyas-wlan.dev:8000',
-]
-LASTUSER_SERVER = 'https://auth.hasgeek.com'
+ALLOWED_ORIGINS = [BASE_URL]
+LASTUSER_SERVER = 'https://hasgeek.com'
 LASTUSER_CLIENT_ID = ''  # nosec
 LASTUSER_CLIENT_SECRET = ''  # nosec
 TIMEZONE = 'Asia/Calcutta'
