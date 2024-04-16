0c43c9def2db4eec22ede3c70df2799c82f040ed
anishTP <anish@hasgeek.com>
2024-04-16 21:51:42 +0530

    Added section for full refund of line-items in the payment receipt

--- boxoffice/views/order.py
+++ boxoffice/views/order.py
@@ -482,11 +482,15 @@ def receipt(order: Order):
     total_base_amount = sum([line_item.base_amount for line_item in line_items])
     total_item_discount = sum([line_item.discounted_amount for line_item in line_items])
     final_amount = sum([line_item.final_amount for line_item in line_items])
+    refunds = []
+    for transaction in order.refund_transactions:
+        refunds.append(transaction)
     return render_template(
         'payment_receipt.html.jinja2',
         total_base_amount=total_base_amount,
         total_item_discount=total_item_discount,
         final_amount=final_amount,
+        refunds=refunds,
         order=order,
         org=order.organization,
         line_items=line_items,
